b86280a030ceb3ba552156981f416a3ecc94d0e4
chinanf-boy <865501259@qq.com>
2018-04-30 15:52:19 +0800

    Up Init

--- cli.js
+++ cli.js
@@ -1,20 +1,84 @@
 #!/usr/bin/env node
-'use strict';
-const meow = require('meow');
-const pathRun = require('.');
+(async () => {
+	"use strict";
+	const meow = require("meow");
+	const path = require("path");
+	const replace = require('replace-in-file');
+	const { pathRun, IsTruePath} = require(".");
 
-const cli = meow(`
+
+
+
+	const cli = meow(`
 	Usage
-	  $ path-run [input]
+	  $ path-run [input] [output]
 
 	Options
-	  --foo  Lorem ipsum [Default: false]
+		input  要更改的路径
+		output 变成的路径
 
 	Examples
-	  $ path-run
-	  unicorns & rainbows
-	  $ path-run ponies
-	  ponies & rainbows
+		$ path-run 'vue' './vue'
+
+	will change all process.cwd()/* files require Path 'vue' => './vue'
 `);
 
-console.log(pathRun(cli.input[0] || 'unicorns'));
+if (!cli.input || cli.input.length !== 2) {
+	console.log(cli.help);
+	process.exit(1);
+}
+
+let In = cli.input[0];
+let Out = cli.input[1];
+
+let InPath = path.resolve(process.cwd(), In)
+let OutPath = path.resolve(process.cwd(), Out)
+
+try{
+
+	IsTruePath(InPath)
+	IsTruePath(OutPath)
+
+	let replaceMesaages = []
+
+	let pathRunMap = await pathRun(InPath, OutPath)
+	let Ks = Object.keys(pathRunMap)
+	for( let i in Ks){
+		let replaceOptions = {}
+		let fileAbs = pathRunMap[Ks[i]].map(f =>{
+			return path.resolve( path.dirname(Ks[i]), f)
+		})
+		// console.log(fileAbs)
+		let Index = fileAbs.indexOf(InPath)
+		if(Index >= 0){ // match Abs path from user InPath
+
+			pathRunMap[Ks[i]][Index] = Out
+
+			// replace content in file
+			replaceOptions = {
+				files: Ks[i],
+				from: new RegExp(`( from)([\\s]+)(\\'|\\")+[`+In+`]+((\\'|\\")+([^\\S;])?)`,'g'),
+				to: ` from '${Out}'`
+			}
+			replaceMesaages.push(replaceOptions)
+			replaceOptions = {
+				files: Ks[i],
+				from: new RegExp(`(require\\()(\\'|\\")[`+In+`]+((\\'|\\")([\\)])+)`,'g'),
+				to: ` require('${Out}')`
+			}
+			replaceMesaages.push(replaceOptions)
+		}
+
+
+	}
+
+	// console.log(pathRunMap)
+	for(let i in replaceMesaages){
+		let changes = await replace(replaceMesaages[i])
+		console.log(changes,"Done")
+	}
+
+}catch(err){
+	throw new Error(err)
+}
+})();
--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+const nodepaths = require("nodepaths")
+
+const globby = require('globby')
+const path = require('path')
+const chalk = require('chalk')
+
+
+
+
+async function pathRun(In, Out){
+
+	let results = {}
+
+	if (typeof In !== 'string') {
+		throw new TypeError(`Expected a string, got ${typeof In}`);
+	}
+	let Files = await globby(path.join(process.cwd(),'*.*'))
+	// console.log(Files)
+	for(let i in Files){
+		results = Object.assign(results, await nodepaths(Files[i]))
 	}
 
-	opts = opts || {};
-
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	return results
 };
+
+function IsTruePath(Abs){
+	try{
+
+		require.resolve(Abs)
+
+	}catch(err){
+		throw new Error(chalk.red('错误路径 ==>>> '+Abs+'\n'))
+	}
+};
+
+module.exports = {
+	IsTruePath,
+	pathRun
+}
