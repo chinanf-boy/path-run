ec9f06b2c737266b3c9305a0902435cc3270176c
chinanf-boy <865501259@qq.com>
2018-05-01 14:11:08 +0800

    Add more index

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 'use strict';
 const nodepaths = require("nodepaths")
-
-const fl = require('node-filelist');
+const relative = require('relative');
 const path = require('path')
 const chalk = require('chalk')
-const NODE_MODULES = '/node_modules'
 
 
-const files   = [ process.cwd() ];
-const fileOption  = { "ext" : "js|jsx|ts|vue" };
 
-async function getAllFiles(files, fileOption){
-	return new Promise((ok,Err)=>{
-		try{
-			fl.read(files, fileOption , function (results){
-				ok(results)
-			});
-		}catch(err){
-			Err(err)
+async function pathRun(options){
+
+	let { InPath, OutPath } = options
+
+	InPath = IsTruePath(InPath)
+	OutPath = IsTruePath(OutPath)
+
+	const files   = options.cwd
+
+	let replaceMesaages = []
+
+	let pathRunMap = await nodepaths(files)
+
+	let Ks = Object.keys(pathRunMap)
+
+	if(Ks.length) // <==== [] will too many for let i in []
+	for( let i in Ks){
+		let replaceOptions = {}
+		let fileAbs = pathRunMap[Ks[i]].map(f =>{
+			try{
+				return require.resolve(path.resolve(path.dirname(Ks[i]), f))
+			}catch(err){
+				return ""
+			}
+		})
+		let Index = fileAbs.indexOf(InPath)
+		if(Index >= 0){ // match Abs path from user InPath
+
+			let fileToInPath = pathRunMap[Ks[i]][Index] // file content require relative path : user
+			let fileToOutPath = relative(Ks[i], OutPath) // file content require relative path : Path-run
+			if(!fileToOutPath.startsWith('.')){
+				fileToOutPath = './'+fileToOutPath
+			}
+			if(fileToInPath === fileToOutPath) continue
+
+			// replace content in file
+			replaceOptions = {
+				files: Ks[i],
+				from: new RegExp(`( from)([\\s]+)(\\'|\\")+[`+fileToInPath+`]+((\\'|\\")+([^\\S;])?)`,'g'),
+				to: ` from '${fileToOutPath}'`
+			}
+			replaceMesaages.push(replaceOptions)
+			replaceOptions = {
+				files: Ks[i],
+				from: new RegExp(`(require\\()(\\'|\\")[`+fileToInPath+`]+((\\'|\\")([\\)])+)`,'g'),
+				to: `require('${fileToOutPath}')`
+			}
+			replaceMesaages.push(replaceOptions)
 		}
-	})
-}
-
-
-async function pathRun(In, Out){
-
-	let results = {}
-
-	if (typeof In !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof In}`);
 	}
-	// let Files = await globby(path.join(process.cwd(),'*.*'))
-
-	let Files = await getAllFiles(files, fileOption)
-
-	Files = Files.map(F =>F.path).filter(x => !x.includes(NODE_MODULES))
-
-	for(let i in Files){
-		results = Object.assign(results, await nodepaths(Files[i]))
-	}
-
-	return results
+	return replaceMesaages
 };
 
 function IsTruePath(Abs){
@@ -49,12 +65,11 @@ function IsTruePath(Abs){
 		return require.resolve(Abs)
 
 	}catch(err){
-		throw new Error(chalk.red('错误路径 ==>>> '+Abs+'\n'))
+		throw new TypeError(chalk.red('错误路径 ==>>> '+Abs+'\n'))
 	}
 };
 
 module.exports = {
 	IsTruePath,
-	pathRun,
-	getAllFiles
+	pathRun
 }
