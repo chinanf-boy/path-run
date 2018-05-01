8b76e025847a7d51f803f5abd4e5563da15dcb9e
chinanf-boy <865501259@qq.com>
2018-05-01 15:28:27 +0800

    Add more test

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,5 @@
 const path = require('path')
+const configJson = require('config.json')
+
 const fromInput1 = require('./output/output3.js')
 
--- demo/input/input1.js
+++ demo/input/input1.js
@@ -1,3 +1,5 @@
 const input2 = require('./input2')
+const fs = require('fs')
+const configJson = require('../config.json')
 
 module.exports = input2
